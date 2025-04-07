d7d254985004f34f6d0650dc5a4c711f999c1d22
Gabriel Couto <gabriel.couto2704@gmail.com>
2025-04-07 11:23:45 -0300

    Preparando component Produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Produtos = () => {
+const Produtos = ({imagem, titulo, descricao, preco}) => {
   return (
-    <div>
-      
-    </div>
+    <article className='produto'>
+      <img src={imagem} alt={titulo}></img>
+      <h3>{titulo}</h3>
+      <p>{descricao}</p>
+      <p className='preco'>{preco}</p>
+      <button>Comprar</button>
+    </article>
   )
 }
 
